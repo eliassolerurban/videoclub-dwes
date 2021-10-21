7ed889dc3aa60d44746d56aaa850073061ecca07
eliassoler <solerurban.elias@gmail.com>
2021-10-21 16:41:41 +0200

    320 teminado

--- src/Soporte.php
+++ src/Soporte.php
@@ -3,27 +3,45 @@
 declare(strict_types=1);
 
 
-class Soporte
-{
-    private static float $iva = 1.21;
-
-    public function __construct(
-
-
-        public string $titulo,
-        protected string $numero,
-        private float $precio
-    ) {
-    }
-    public function getPrecio() : float
-    {
-
-        return  $this->precio;
-    }
-    public function getPrecioConIva(): float
-    {
-
+class Soporte {
     
-    return  $this->$this->precio * self::iva;
-    }
+  private static float $iva = 1.21;
+
+  public function __construct(
+
+
+    public string $titulo,
+    protected string $numero,
+    private float $precio
+  ) {
+  
+  }
+  
+  public function getPrecio() : float {
+
+    return  $this->precio;
+  
+  }
+  
+  public function getPrecioConIVA(): float {
+    
+    return  $this->precio * self::$iva;
+    
+  }
+
+  public function getNumero(): string{
+
+    return $this->numero;
+
+  }
+
+  public function muestraResumen() {
+    
+    echo "<br>";
+    echo $this->titulo;
+    echo "<br>";
+    echo $this->precio . " (IVA no incluido)";
+    
+  }
+  
 }
