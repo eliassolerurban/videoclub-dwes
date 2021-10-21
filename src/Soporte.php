efb8fe84395874265649312b7d01f03c25b2c313
eliassoler <solerurban.elias@gmail.com>
2021-10-21 16:31:32 +0200

    movido Soporte a src

--- src/Soporte.php
+++ src/Soporte.php
@@ -1,17 +1,29 @@
-<?php 
+<?php
+
 declare(strict_types=1);
 
 
-class Soporte {
-    private static float $iva=1.21;
+class Soporte
+{
+    private static float $iva = 1.21;
 
     public function __construct(
 
-       
+
         public string $titulo,
         protected string $numero,
         private float $precio
     ) {
-  } 
+    }
+    public function getPrecio() : float
+    {
 
+        return  $this->precio;
+    }
+    public function getPrecioConIva(): float
+    {
+
+    
+    return  $this->$this->precio * self::iva;
+    }
 }
