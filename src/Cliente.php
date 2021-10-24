2cdfd3d1bc9abbfa79eba402ba27c138f34440d2
Elías <solerurban.elias@gmail.com>
2021-10-24 17:55:11 +0200

    ehcho hasta listarAlquileres, falta cuadrarlo con inicio

--- src/Cliente.php
+++ src/Cliente.php
@@ -37,14 +37,38 @@ class Cliente {
         return isset($this->soportesAlquilados[$s->getNumero()]); //falta revisar
      }
 
-     public function alquilar(Soporte $s): bool{
+     public function alquilar(Soporte $s): bool {
         if(!$this->tieneAlquilado($s) and count($this->soportesAlquilados) < $this->maxAlquilerConcurrente){
             $this->soportesAlquilados[] = $s;
             $this->numSoportesAlquilados++;
             
+            echo "El soporte se ha alquilado correctamente";
             return true;
         }
 
+        echo "No se ha podido alquilar el soporte";
         return false;
+    }
+
+    public function devolver(int $numSoporte): bool {
+        if($this->tieneAlquilado($this->soportesAlquilados[$numSoporte])){
+            unset($this->soportesAlquilados[$numSoporte]);
+            $this->numSoportesAlquilados--;
+
+            echo "El soporte se ha devueldo correctamente";
+            return true;
+        }
+
+        echo "El soporte no se ha podido devolver";
+        return false;
+    }
+
+    public function listarAlquileres(): void{
+        echo "$this->nombre cuenta con $this->numSoportesAlquilados alquileres";
+        echo "";
+        echo "Los alquileres son:";
+        foreach($this->soportesAlquilados as $soportesAlquilado){
+            echo $soportesAlquilado->titulo;
+        }
     }
 }
