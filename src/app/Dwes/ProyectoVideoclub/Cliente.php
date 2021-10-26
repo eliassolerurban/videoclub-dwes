0554685b7bd82e71eafe9a6eb8315495044fcb94
issam bezzaz <bezzazissam3@gmail.com>
2021-10-26 16:54:12 +0200

    puesto excetion cliente y videoclub

--- src/app/Dwes/ProyectoVideoclub/Cliente.php
+++ src/app/Dwes/ProyectoVideoclub/Cliente.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace Dwes\ProyectoVideoclub;
 
-class Cliente {
+use Dwes\ProyectoVideoclub\Util\CupoSuperadoException;
+use Dwes\ProyectoVideoclub\Util\SoporteNoEncontradoException;
+use Dwes\ProyectoVideoclub\Util\SoporteYaAlquiladoException;
+
+class Cliente
+{
 
     public array $soportesAlquilados;
     private int $numSoportesAlquilados;
@@ -18,76 +23,76 @@ class Cliente {
         $this->numSoportesAlquilados = 0;
     }
 
-    public function setNumero(int $numero): Cliente {
+    public function setNumero(int $numero): Cliente
+    {
         $this->numero = $numero;
         return $this;
     }
 
-    public function getNumero(): int {
+    public function getNumero(): int
+    {
         return $this->numero;
     }
 
-    public function getNumSoportesAlquilados(): int {
-        return $this-> numSoportesAlquilados;
+    public function getNumSoportesAlquilados(): int
+    {
+        return $this->numSoportesAlquilados;
     }
 
-     public function muestraResumen(){
-         echo $this->nombre;
-         echo count($this->soportesAlquilados);
-     }
+    public function muestraResumen()
+    {
+        echo $this->nombre;
+        echo count($this->soportesAlquilados);
+    }
 
-     public function tieneAlquilado(Soporte $s): bool {
+    public function tieneAlquilado(Soporte $s): bool
+    {
         return isset($this->soportesAlquilados[$s->getNumero()]);
-     }
+    }
 
-     public function alquilar(Soporte $s): Cliente {
-        
-        if(!$this->tieneAlquilado($s)){
-            if($this->numSoportesAlquilados < $this->maxAlquilerConcurrente){
+    public function alquilar(Soporte $s): Cliente
+    {
+
+        if (!$this->tieneAlquilado($s)) {
+            if ($this->numSoportesAlquilados < $this->maxAlquilerConcurrente) {
                 $this->soportesAlquilados[$s->getNumero()] = $s;
                 $this->numSoportesAlquilados++;
-                
+
                 echo "<br>";
                 echo "<br>";
                 echo "<strong>Alquilado soporte a: </strong> $this->nombre";
                 echo "<br>";
                 echo $s->muestraResumen();
                 return $this;
+            } else {
+                throw new CupoSuperadoException("CupoSuperadoException: El cliente tiene $this->numSoportesAlquilados elementos alquilados.");
             }
-
-            else {
-                echo "<br>";
-                echo "El cliente tiene $this->numSoportesAlquilados elementos alquilados.
-                    No puede alquilar más en este videoclub hasta que no devuelva alguno.";
-            }
-        }
-
-        else {
-            echo "<br>";
-            echo "El cliente ya tiene alquilado el soporte <strong>$s->titulo</strong>";
+        } else {
+            throw new SoporteYaAlquiladoException("SoporteYaAlquiladoException: El cliente ya tiene alquilado el soporte <strong>$s->titulo</strong>");
         }
 
         return $this;
     }
 
-    public function devolver(int $numSoporte): bool {
-        if($this->numSoportesAlquilados != 0){
+    public function devolver(int $numSoporte): bool
+    {
+        if ($this->numSoportesAlquilados != 0) {
             unset($this->soportesAlquilados[$numSoporte]);
             $this->numSoportesAlquilados--;
 
             echo "El soporte se ha devueldo correctamente";
             return true;
         }
-
-        echo "No se ha encontrado el soporte en los alquileres de este cliente.";
+        throw new SoporteNoEncontradoException("SoporteNoEncontradoException: No se ha encontrado el soporte en los alquileres de este cliente.");
         return false;
     }
 
-    public function listarAlquileres(): void{
+    public function listarAlquileres(): void
+    {
         echo "$this->nombre cuenta con $this->numSoportesAlquilados alquileres";
         echo "";
         echo "Los alquileres son:";
-        foreach($this->soportesAlquilados as $soportesAlquilado){
+        foreach ($this->soportesAlquilados as $soportesAlquilado) {
             echo $soportesAlquilado->titulo;
         }
     }
---
+++ src/app/Dwes/ProyectoVideoclub/Util/CupoSuperadoException.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dwes\ProyectoVideoclub\Util;
+
+include_once("VideoclubException.php");
+
+class CupoSuperadoException extends VideoclubException
+{
+
+  
+}
