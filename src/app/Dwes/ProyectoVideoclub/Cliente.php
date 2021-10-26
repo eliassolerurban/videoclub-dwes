aaecf30a4f3be0b7db73de1c4ed69825c7c7279a
eliassoler <solerurban.elias@gmail.com>
2021-10-26 17:55:06 +0200

    detectado error en linea 58 de cliente

--- src/app/Dwes/ProyectoVideoclub/Cliente.php
+++ src/app/Dwes/ProyectoVideoclub/Cliente.php
@@ -53,7 +53,7 @@ class Cliente
 
     public function alquilar(Soporte $s): Cliente
     {
-
+        //fatal error, duda
         if ($this->tieneAlquilado($s)) {
             throw new SoporteYaAlquiladoException("SoporteYaAlquiladoException: El cliente ya tiene alquilado el soporte <strong>$s->titulo</strong><br>");
         }
