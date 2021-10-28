b19f46fadffb12d07e0d3e0a30dbdf55a2577695
eliassoler <solerurban.elias@gmail.com>
2021-10-28 15:35:48 +0200

    añadido trycatch a inicioVideoclub

--- src/inicioVideoclub.php
+++ src/inicioVideoclub.php
@@ -1,5 +1,7 @@
 <?php
 include_once("vendor/autoload.php"); // No incluimos nada más
+
+use Dwes\ProyectoVideoclub\Util\VideoclubException;
 use Dwes\ProyectoVideoclub\Videoclub;
 $vc = new Videoclub("Severo 8A"); 
 
@@ -12,19 +14,24 @@ $vc->incluirJuego("God of War", 19.99, "PS4", 1, 1)
     ->incluirCintaVideo("Los cazafantasmas", 3.5, 107)
     ->incluirCintaVideo("El nombre de la Rosa", 1.5, 140); 
 
-//listo los productos 
-$vc->listarProductos(); 
+try{
+    //listo los productos 
+    $vc->listarProductos(); 
 
-//voy a crear algunos socios 
-$vc->incluirSocio("Amancio Ortega")->incluirSocio("Pablo Picasso", 2); 
+    //voy a crear algunos socios 
+    $vc->incluirSocio("Amancio Ortega")->incluirSocio("Pablo Picasso", 2); 
 
-$vc->alquilaSocioProducto(1,2)->alquilaSocioProducto(1,3); 
-//alquilo otra vez el soporte 2 al socio 1. 
-// no debe dejarme porque ya lo tiene alquilado 
-$vc->alquilaSocioProducto(1,2); 
-//alquilo el soporte 6 al socio 1. 
-//no se puede porque el socio 1 tiene 2 alquileres como máximo 
-$vc->alquilaSocioProducto(1,6); 
+    $vc->alquilaSocioProducto(1,2)->alquilaSocioProducto(1,3); 
+    //alquilo otra vez el soporte 2 al socio 1. 
+    // no debe dejarme porque ya lo tiene alquilado 
+    $vc->alquilaSocioProducto(1,2); 
+    //alquilo el soporte 6 al socio 1. 
+    //no se puede porque el socio 1 tiene 2 alquileres como máximo 
+    $vc->alquilaSocioProducto(1,6); 
 
-//listo los socios 
-$vc->listarSocios();
+    //listo los socios 
+    $vc->listarSocios();
+}
+catch(VideoclubException $e){
+    echo "Se ha producido un error: " . $e->getMessage();
+}
